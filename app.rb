be389335d1e60d6b19b9d13d8c936f28df6337b2
Ajay <ajaymungur@hotmail.com>
2018-11-15 12:10:00 +0000

    Used TDD to add in user input for name, day and month

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
 
 class BirthdayGreeter < Sinatra::Base
+
   get '/' do
-    "Happy Birthday!!"
+    erb :index
+  end
+
+  post '/index' do
+    @name = params[:name]
+    @day = params[:day]
+    @month = params[:month]
+    erb :greet
   end
 
 !run if app_file == $0
--- spec/features/infrastructure_test_spec.rb
+++ spec/features/infrastructure_test_spec.rb
@@ -1,7 +1,27 @@
 
-feature 'Testing infrastructure' do
-  scenario 'can run app and check page content' do
+feature 'Title testing' do
+  scenario 'can run app and page contais heading' do
     visit ('/')
-    expect(page).to have_content 'Happy Birthday!!'
+    expect(page).to have_content 'Hello there!!'
+  end
+end
+
+feature 'Testing name input' do
+  scenario 'can run app, enter name and name will be returned' do
+    visit ('/')
+    fill_in 'name', with: 'Ajay'
+    click_button 'Submit'
+    expect(page).to have_content 'Ajay!!'
+  end
+end
+
+feature 'Testing name inout' do
+  scenario 'can run app enter name and name will be returned' do
+    visit ('/')
+    fill_in 'name', with: 'Ajay'
+    fill_in 'day', with: 23
+    select 'April', from: "month"
+    click_button 'Submit'
+    expect(page).to have_content 'Ajay!! 23 April'
   end
 end
